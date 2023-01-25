0b63b6de6361e60471d57d14691c4a752c25067e
Satyam7Jha <7satyamjha@gmail.com>
2023-01-25 10:55:08 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div id="main-container" className=" h-[100vh] w-[100%]">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
